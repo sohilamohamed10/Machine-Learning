127c0b3034be7e8a63678135b51b9433b6c6b7df
unknown <sohila.m@outlook.com>
2022-09-27 00:53:40 +0200

    added readme

--- linear_regression/GradientDescent.py
+++ linear_regression/GradientDescent.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 class regression():
     def __init__(self,data):
        self.features=None
@@ -15,19 +17,20 @@ class regression():
         
     def cost_function(self,y_pred): 
         #loss=∑(y-h)^2
-        cost_fn=np.sum(np.power((self.y-y_pred),2))
+        cost_fn=(np.sum(np.power((y_pred-self.y),2)))/(2*len(y_pred))
         return cost_fn
 
     def gradient_descent(self,y_pred,lrate):
         #dw , db
-        gradient=-2*np.dot((self.y-y_pred),self.features)     
-        gradient_bias=-2*np.sum(self.y-y_pred)
+        diff=y_pred-self.y
+        gradient=(np.dot((diff),self.features))/len(y_pred)   
+        gradient_bias=(np.sum(diff))/len(y_pred)
         self.weights=self.weights-lrate*gradient
         self.bias=self.bias-lrate*gradient_bias
         y_pred_new=self._predict(self.features)
         return y_pred_new
 
-    def fit(self,x,y,lrate=0.01): 
+    def fit(self,x,y,lrate=0.000001): 
         self.features=x
         self.y=y
         #normalization if data is multivariate
@@ -38,18 +41,19 @@ class regression():
         cost=self.cost_function(y_pred)
         self.cost_fns.append(cost)
         c=1
-        while(True):
+        while (True):
             y_pred=self.gradient_descent(y_pred,lrate)
             self.cost_fns.append(self.cost_function(y_pred))
             #if cost fn is increasing reduce lr
-            if (self.cost_fns[c]>self.cost_fns[c-1]):
-                lrate=lrate*0.2
+            # if (self.cost_fns[c]>self.cost_fns[c-1]):
+            #     lrate=lrate*0.5
                 #terminate when cost fn is almost constant
             if(abs(self.cost_fns[c]-self.cost_fns[c-1])<0.0001):
-                print(self.cost_fns[c])
+                #print(self.cost_fns[c])
                 break
             c=c+1
-
+    
+        
     def predict(self,X_test):
         #normalization of test data in multivariate data
         if(X_test.shape[1]>1):
@@ -61,18 +65,18 @@ class regression():
         #mean absolute error mertic
         for i in range(len(y)) :
             diff=y[i]-y_true[i]
-            error.append(abs((y[i] - y_true[i])/y_true[i]))
-        accuracy=(np.sum(error)/len(y))
-        return accuracy*100
+            error.append((y[i] - y_true[i])**2)
+        mse=(np.sum(error)/len(y))
+        return mse
  
  #-------------------------------
  #example                
-data= pd.read_table('multivariateData.dat' ,sep=",",header=None)
+data= pd.read_table('data/univariateData.dat' ,sep=",",header=None)
 x=data.iloc[:,:-1].to_numpy()
 y=data.iloc[:,-1].to_numpy()
 r=regression(data)
 X_train, X_test, y_train,y_test = train_test_split(x, y,test_size=0.2)
 r.fit(X_train,y_train)
 predictions=r.predict(X_test)
-accuracy=r.evaluate(predictions,y_test)
-print(accuracy)
+error=r.evaluate(predictions,y_test)
+print(error)
---
+++ linear_regression/README.md
@@ -0,0 +1,9 @@
+# Linear Regression model
+
+## Description:
+
+
+The code applies gradient descent algorithm ,assume initial values for 
+weights and bias , calculate y and then optimize the parameters to reduce the cost function , learning rate it update to be smaller if cost is getting higher. I assumed termination will be when cost function is almost constant it can be also done through specific number of iterations.
+Data is normalized if the dataset is multivariate .
+Evaluating performance used mean square error metric calculating error.
